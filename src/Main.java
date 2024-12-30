b97b25978ceddf9dd278600595a0a2019be5cddb
dejan-marlovic <dmarlovic83@gmail.com>
2024-12-30 12:28:13 +0100

    Changed to HashMap for storing accounts

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
        long account1 = Bank.openAccount("Dejan Marlovic");
        long account2 = Bank.openAccount("Nils Jacobsen");
-        Bank.accounts.getFirst().deposit(3999);
-        Bank.accounts.get(1).deposit(5533);
-        Bank.accounts.get(1).withdrawal(3000);
+        Bank.accounts.get(account1).deposit(3999);
+        Bank.accounts.get(account1).deposit(5533);
+        Bank.accounts.get(account2).withdrawal(3000);
 
         Bank.printAccountInfo();
 
