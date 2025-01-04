82fc9d870e2e6a79a00057620ee96256f9185dc5
dejan-marlovic <dmarlovic83@gmail.com>
2025-01-04 22:55:00 +0100

    Testing negative transactions

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Long account2 = Bank.openAccount("Nils Jacobsen");
 
         try {
-            Bank.getAccount(account1).deposit(-50);
+            Bank.getAccount(account1).deposit(5000);
         }catch (IllegalArgumentException e)
         {
             System.out.println(e.getMessage());
@@ -14,7 +14,7 @@ public class Main {
 
             Bank.getAccount(account2).deposit(5533);
 
-        Bank.accountTransfer(account1, account2, 2000.45);
+        Bank.accountTransfer(account1, account2, -10);
 
         Bank.printAccountInfo();
 
